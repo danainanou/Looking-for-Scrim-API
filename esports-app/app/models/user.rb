9e996e9a724e128ff49dadf3235b62d9019594f4
danainanou <danai.nanou@gmail.com>
2017-06-05 16:31:42 +0100

    added roles to user model

--- esports-app/app/models/user.rb
+++ esports-app/app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  enum role: [:player, :team]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :player
+  end
   has_many :posts
   validates :username, presence: true, uniqueness: true
 end
